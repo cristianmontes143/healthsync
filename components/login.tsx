f23130154b42b9e41d7910cbe5adeaca90044f98
cristianmontes143 <crischuychuyz@gmail.com>
2024-01-25 11:20:43 +0800

    button

---
+++ components/login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './ui/button';
+
+const Login = () => {
+  return (
+    <>
+        <div>L</div>
+        <Button className='bg-green-500'>dfsfds</Button>
+    </>
+  )
+}
+
+export default Login;
